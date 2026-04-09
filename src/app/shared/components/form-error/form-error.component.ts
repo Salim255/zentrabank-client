98f9b7a4e8400b31adefe98939268f44cf987910
Salim <doshka23@gmail.com>
2026-04-09 20:23:16 +0200

    built: form error

---
+++ src/app/shared/components/form-error/form-error.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+import { FORM_ERRORS } from '../../utils/form-errors';
+
+
+@Component({
+  selector: 'app-form-error',
+  templateUrl: './form-error.component.html'
+})
+export class FormErrorComponent {
+  @Input() control!: AbstractControl | null;
+  @Input() field!: string;
+
+  get errorMessage(): string | null {
+    if (!this.control || !this.control.errors) return null;
+
+    const errors = this.control.errors;
+    const fieldErrors = FORM_ERRORS[this.field];
+
+    for (const key of Object.keys(errors)) {
+      if (fieldErrors && fieldErrors[key]) {
+        return fieldErrors[key];
+      }
+    }
+
+    return 'Invalid field';
+  }
+}
