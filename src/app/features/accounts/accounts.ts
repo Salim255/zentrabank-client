4da2cb05c2081af43c9300195646111aef42d560
Salim <doshka23@gmail.com>
2026-04-10 02:12:34 +0200

    added: Accounts feature

---
+++ src/app/features/accounts/accounts.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-accounts",
+    templateUrl: "./accounts.html",
+    styleUrls: ["./accounts.scss"],
+    standalone: false
+})
+export class Accounts {
+  constructor() {}
+}
