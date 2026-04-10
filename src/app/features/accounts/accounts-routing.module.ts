375dad1602d7a9e9a63fe0e8a6ce280bdd8b0739
Salim <doshka23@gmail.com>
2026-04-10 02:59:23 +0200

    added:AccountHeaderComponent

--- src/app/features/accounts/accounts-routing.module.ts
+++ src/app/features/accounts/accounts-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { Accounts } from "./accounts";
+import { Details } from "./pages/details/details";
 
 const routes:Routes = [
   {
     path: "",
     component: Accounts,
     children: [
-
+      {
+        path: "details",
+        component: Details
+      }
     ]
   }
 ]
