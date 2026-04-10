6901f85199155ff0edaaff44c564a0d8d59affea
Salim <doshka23@gmail.com>
2026-04-10 12:03:59 +0200

    added:  Analytics

---
+++ src/app/features/analytics/analytics.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-analytics",
+  templateUrl: "./analytics.html",
+  styleUrls: ["./analytics.scss"],
+  standalone: false
+})
+export class Analytics {}
