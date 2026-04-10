ab59419fcec5cc0bb69970c4dc0bac09d721f05b
Salim <doshka23@gmail.com>
2026-04-10 15:31:53 +0200

    built: WithdrawPage

---
+++ src/app/features/transactions/pages/create-transaction/pages/withdraw/withdraw.page.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-withdraw",
+  templateUrl: "./withdraw.page.html",
+  styleUrls: ["./withdraw.page.html"],
+  standalone: false
+})
+export class WithdrawPage{}
