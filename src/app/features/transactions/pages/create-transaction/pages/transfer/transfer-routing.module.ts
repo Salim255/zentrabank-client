4066a84e3169174a5d0a92f4b35d3851488f0aac
Salim <doshka23@gmail.com>
2026-04-10 15:20:55 +0200

    added: TransferRoutingModule

---
+++ src/app/features/transactions/pages/create-transaction/pages/transfer/transfer-routing.module.ts
@@ -0,0 +1,17 @@
+import { RouterModule, Routes } from "@angular/router";
+import { TransferPage } from "./transfer.page";
+import { NgModule } from "@angular/core";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: TransferPage
+  }
+]
+
+  NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+  })
+
+  export class TransferRoutingModule {}
