85df724e7f52ee040e18bc2dcf550ac7e7e61c89
Salim <doshka23@gmail.com>
2026-04-10 13:21:54 +0200

    added:  AccountModul

---
+++ src/app/features/accounts/pages/account/account.page.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { AccountRoutingModule } from "./account-routing.module";
+import { AccountPage } from "./account.page";
+
+@NgModule({
+  imports: [AccountRoutingModule],
+  declarations: [AccountPage]
+})
+
+export class AccountModule {}
