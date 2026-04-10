9a1749aa466d70eab5401f93016ce82b7e596967
Salim <doshka23@gmail.com>
2026-04-10 15:17:48 +0200

    added: TransferPage

---
+++ src/app/features/transactions/pages/create-transaction/pages/transfer/transfer.page.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-transfer",
+  templateUrl: "./transfer.page.html",
+  styleUrls:["./transfer.page.scss"],
+  standalone: false
+})
+export class TransferPage {}
