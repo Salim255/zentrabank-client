296a07cfc86491e078629dd21732b3f8458282fd
Salim <doshka23@gmail.com>
2026-04-10 14:16:05 +0200

    built: TransactionsHistoryPage

---
+++ src/app/features/transactions/pages/transactions-history/transactions-history.page.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-transactions-history",
+  templateUrl: "./transactions-history.page.html",
+  styleUrls: ["./transactions-history.page.scss"],
+  standalone: false
+})
+export class TransactionsHistoryPage {}
