f575a868186b7721855c95d364c7c7312413e784
Salim <doshka23@gmail.com>
2026-04-10 13:48:27 +0200

    built: transactionHeaderComponent

---
+++ src/app/features/transactions/components/transaction-header/transaction-header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-transaction-header",
+  templateUrl: "./transaction-header.component.html",
+  styleUrls: ["./transaction-header.component.scss"],
+  standalone: false
+})
+export class transactionHeaderComponent {}
