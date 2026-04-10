197478e59197ff5b40df6424074aadd4628b615f
Salim <doshka23@gmail.com>
2026-04-10 15:33:58 +0200

    built: WithdrawRoutingModule

---
+++ src/app/features/transactions/pages/create-transaction/pages/withdraw/withdraw-routing.module.ts
@@ -0,0 +1,17 @@
+import { RouterModule, Routes } from "@angular/router";
+import { WithdrawPage } from "./withdraw.page";
+import { NgModule } from "@angular/core";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: WithdrawPage
+  }
+]
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+
+export class WithdrawRoutingModule{}
