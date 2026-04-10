fa80bdc04fde4737c1aec61026e7284de7daed9c
Salim <doshka23@gmail.com>
2026-04-10 14:48:24 +0200

    built: TransOptionsPage

---
+++ src/app/features/transactions/pages/create-transaction/pages/trans-options/trans-options-routing.module.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { TransOptionsPage } from "./trans-options.page";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: TransOptionsPage
+  }
+]
