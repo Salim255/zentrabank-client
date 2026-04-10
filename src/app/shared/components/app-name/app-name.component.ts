e0afdd4a0b6deb9a65bd4e94449d88916b1b9bc7
Salim <doshka23@gmail.com>
2026-04-10 03:45:05 +0200

    built: app logo

---
+++ src/app/shared/components/app-name/app-name.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-name",
+  templateUrl: "./app-name.component.html",
+  styleUrls: ["./app-name.component.scss"],
+  standalone: false
+})
+export class AppNameComponent {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { FormErrorComponent } from "./components/form-error/form-error.component";
 import { CommonModule } from "@angular/common";
+import { AppNameComponent } from "./components/app-name/app-name.component";
 
 @NgModule({
-  declarations: [FormErrorComponent],
+  declarations: [FormErrorComponent, AppNameComponent],
   imports: [CommonModule],
-  exports: [FormErrorComponent]
+  exports: [FormErrorComponent, AppNameComponent]
 })
 export class SharedModule {}
