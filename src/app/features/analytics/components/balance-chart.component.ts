d78be60d367ce3b53b354d9d5ff723260d6af874
Salim <doshka23@gmail.com>
2026-04-10 12:25:01 +0200

    built:BalanceChartComponent

--- src/app/features/analytics/components/balance-chart.component.ts
+++ src/app/features/analytics/components/balance-chart.component.ts
@@ -1,1 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-balance-chart",
+  templateUrl: "./balance-chart.component.html",
+  styleUrls: ["./balance-chart.component.scss"],
+  standalone: false
+})
 export class BalanceChartComponent{}
