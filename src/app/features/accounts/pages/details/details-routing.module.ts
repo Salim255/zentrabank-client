1a770907704b5ab17fcc826af3c2a3c19e25ce27
Salim <doshka23@gmail.com>
2026-04-10 02:55:02 +0200

    built:  DetailsModule

---
+++ src/app/features/accounts/pages/details/details-routing.module.ts
@@ -0,0 +1,17 @@
+import { RouterModule, Routes } from "@angular/router";
+import { Details } from "./details";
+import { NgModule } from "@angular/core";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: Details
+  }
+]
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+
+export class DetailsModule {}
