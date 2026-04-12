bff03992c4d8c281822627a679435e4ec506819c
Salim <doshka23@gmail.com>
2026-04-12 19:56:12 +0200

    updated: env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:8080/api/v1' // Replace with actual production API URL
+  // apiBaseUrl: 'http://localhost:8080/api/v1' // Replace with actual production API URL
+  apiBaseUrl:"https://api.zentrabank.salimcode.site/api/v1/"
 };
