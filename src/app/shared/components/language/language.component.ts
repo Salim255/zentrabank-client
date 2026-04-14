6345a53972d7bdfc0abedf027483a1158b55f585
Salim <doshka23@gmail.com>
2026-04-14 18:30:15 +0200

    added: LanguageComponent

---
+++ src/app/shared/components/language/language.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-language",
+  templateUrl: "./language.component.html",
+  styleUrls: ["./language.component.scss"],
+  standalone: false
+})
+export class LanguageComponent {}
