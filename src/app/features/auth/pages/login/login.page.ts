2edfcd581d9928f26a87d35819b1770594b8b871
Salim <doshka23@gmail.com>
2026-04-14 22:36:14 +0200

    built: language selector style

--- src/app/features/auth/pages/login/login.page.ts
+++ src/app/features/auth/pages/login/login.page.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
   standalone: false
 })
 export class LoginPage {}
