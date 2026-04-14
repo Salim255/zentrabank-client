155224fea2764d0f9ea0998d9226e31357c2564e
Salim <doshka23@gmail.com>
2026-04-14 18:40:47 +0200

    built: auth layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,19 @@ import { FormErrorComponent } from "./components/form-error/form-error.component
 import { CommonModule } from "@angular/common";
 import { AppNameComponent } from "./components/app-name/app-name.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { LanguageComponent } from "./components/language/language.component";
 
 @NgModule({
-  declarations: [FormErrorComponent,NavbarComponent, AppNameComponent],
+  declarations: [
+    FormErrorComponent,
+    NavbarComponent,
+    AppNameComponent,
+    LanguageComponent],
   imports: [CommonModule],
-  exports: [FormErrorComponent, AppNameComponent, NavbarComponent]
+  exports: [
+    FormErrorComponent,
+    AppNameComponent,
+     NavbarComponent,
+     LanguageComponent]
 })
 export class SharedModule {}
