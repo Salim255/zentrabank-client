3e3362e53de197dada17ec8604a7262f0f89fa66
Salim <doshka23@gmail.com>
2026-04-19 00:19:23 +0200

    built: signup routing

--- src/app/features/auth/pages/signup/signup-routing.module.ts
+++ src/app/features/auth/pages/signup/signup-routing.module.ts
@@ -1,11 +1,28 @@
 import { RouterModule, Routes } from "@angular/router";
 import { SignupPage } from "./signup.page";
 import { NgModule } from "@angular/core";
+import { SignupFormComponent } from "./components/signup-form/signup-form.component";
+import { CreateAccountFormComponent } from "../../../accounts/components/create-account-form/create-account-form.component";
 
 const routes: Routes = [
   {
     path: "",
-    component: SignupPage
+    component: SignupPage,
+    children: [
+       {
+        path: '',
+        redirectTo: 'register',
+        pathMatch: 'full'
+      },
+      {
+        path: "register",
+        component: SignupFormComponent
+      },
+      {
+        path: "application",
+        component: CreateAccountFormComponent
+      }
+    ]
   }
 ]
 
