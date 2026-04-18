5e9e0a821e9051bb354ad2f910176037801d8178
Salim <doshka23@gmail.com>
2026-04-19 00:06:03 +0200

    built: signup module

---
+++ src/app/features/auth/pages/signup/signup.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { SignupRoutingModule } from "./signup-routing.module";
+import { SignupPage } from "./signup.page";
+
+@NgModule({
+  declarations: [SignupPage],
+  imports: [SignupRoutingModule]
+})
+
+export class SignupModule {}
