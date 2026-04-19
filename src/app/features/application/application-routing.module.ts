88fbd9168f769cdc564fb7034172d4837243371d
Salim <doshka23@gmail.com>
2026-04-19 02:04:39 +0200

    refactored: application routing

--- src/app/features/application/application-routing.module.ts
+++ src/app/features/application/application-routing.module.ts
@@ -9,8 +9,9 @@ const routes: Routes = [
     path: "",
     component: ApplicationPage,
     children: [
-       {
-        path: "application",
+      {
+        path: "",
+        pathMatch: "full",
         component: CreateAccountFormComponent
       },
       {
