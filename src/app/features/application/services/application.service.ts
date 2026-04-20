362548ffb3b249893d93591985db16497ebbb1f6
Salim <doshka23@gmail.com>
2026-04-21 00:29:41 +0200

    built: ApplicationService

--- src/app/features/application/services/application.service.ts
+++ src/app/features/application/services/application.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import { ApplicationInstance } from "../model/application.model";
 
 @Injectable({providedIn: "root"})
-export class ApplicationService {}
+export class ApplicationService {
+  private applicationInstanceSubject = new BehaviorSubject<ApplicationInstance | null>(null);
+  applicationInstanceSubject$ = this.applicationInstanceSubject.asObservable();
+
+}
