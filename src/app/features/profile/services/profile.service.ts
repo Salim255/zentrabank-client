4b77a8a76f6162a4e8c3c91d4ab40cb265957115
Salim <doshka23@gmail.com>
2026-04-21 20:19:17 +0200

    built: setProfile

--- src/app/features/profile/services/profile.service.ts
+++ src/app/features/profile/services/profile.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import { Profile } from "../model/profile.model";
 
 @Injectable({providedIn: "root"})
-export class ProfileService {}
+export class ProfileService {
+  private profileSubject = new BehaviorSubject<Profile | null>(null);
+  profileSubject$ = this.profileSubject.asObservable();
+
+  constructor(){}
+
+  setProfile(profile: Profile | null){
+    this.profileSubject.next(profile);
+  }
+}
