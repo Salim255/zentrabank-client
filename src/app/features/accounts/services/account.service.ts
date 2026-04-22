faf62d1fbe238ead08a9836f23da7539a6568a37
Salim <doshka23@gmail.com>
2026-04-22 13:57:58 +0200

    built: setAccounts

--- src/app/features/accounts/services/account.service.ts
+++ src/app/features/accounts/services/account.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from "@angular/core";
 import { AccountHttpService } from "./account-http.service";
-import { Observable } from "rxjs";
+import { BehaviorSubject, Observable } from "rxjs";
+import { Account } from "../model/account.model";
 
 @Injectable({providedIn: "root"})
 export class AccountService {
+  private accountsSubject = new BehaviorSubject<Account [] >([])
+
+
   constructor(private accountHttpService: AccountHttpService){}
+
+  setAccounts(accounts: Account[]): void{
+    this.accountsSubject.next(accounts);
+  }
 
   getUserAccounts(): Observable<any>{
     return this.accountHttpService.fetchAccounts()
