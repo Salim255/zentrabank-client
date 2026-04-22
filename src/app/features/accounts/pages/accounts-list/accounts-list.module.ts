a4c5e6ad18ce39cf2ed10949697b30dd4712ceee
Salim <doshka23@gmail.com>
2026-04-22 15:33:33 +0200

    built: AccountsListModule

---
+++ src/app/features/accounts/pages/accounts-list/accounts-list.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { AccountsListRoutingModule } from "./accounts-list-routing.module";
+import { AccountsListPage } from "./accounts-list.page";
+
+@NgModule({
+  imports: [AccountsListRoutingModule],
+  declarations: [AccountsListPage]
+})
+
+export class AccountsListModule {
+
+}
