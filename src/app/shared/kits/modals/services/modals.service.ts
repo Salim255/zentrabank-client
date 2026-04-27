19223dd780f04399edfb34ec173e9a3f8e31d7f8
Salim <doshka23@gmail.com>
2026-04-27 12:23:50 +0200

    built: bg modal component

--- src/app/shared/kits/modals/services/modals.service.ts
+++ src/app/shared/kits/modals/services/modals.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from "@angular/core";
+import { ModalController } from "@ionic/angular";
 
 @Injectable({providedIn: "root"})
-export class ModalsService {}
+export class ModalsService {
+  constructor(private mdlController: ModalController){}
+
+}
