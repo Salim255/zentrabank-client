1511ba86f18691357df0d5a3a52101d0baa60e15
Salim <doshka23@gmail.com>
2026-04-27 16:26:25 +0200

    built: ProfileRoutingModule

---
+++ src/app/features/profile/profile-routing.module.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { ProfilePage } from "./profile.page";
+
+const routes: Routes = [ {
+    path: "",
+    component: ProfilePage
+}]
+
+export class ProfileRoutingModule{}
--- src/app/features/profile/profile.page.ts
+++ src/app/features/profile/profile.page.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
+import { AccountRoutingModule } from "../account/account-routing.module";
 
 @Component({
   selector: "app-profile",
   templateUrl: "./profile.page.html",
-  styleUrl: "./profile.page.scss"
+  styleUrl: "./profile.page.scss",
+  imports: [AccountRoutingModule]
 })
 export class ProfilePage{}
