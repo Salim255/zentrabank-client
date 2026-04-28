7408eb08ba01cfd5072532f1c01d11f27d4ec949
Salim <doshka23@gmail.com>
2026-04-28 16:26:00 +0200

    built: subscribeToTransactions

--- src/app/features/transactions/pages/transactions-history/services/history.service.ts
+++ src/app/features/transactions/pages/transactions-history/services/history.service.ts
@@ -1,6 +1,24 @@
 import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable } from "rxjs";
+import { HistoryHttpService } from "./history-http.service";
 
 @Injectable({providedIn: "root"})
 export class HistoryService {
-  constructor(){}
+
+  private transactionsHistorySubject = new BehaviorSubject<any>(null);
+
+  constructor(private historyHttpService: HistoryHttpService){}
+
+
+  get getTransactions$(): Observable<any>{
+    return this.transactionsHistorySubject.asObservable();
+  }
+
+  setTransactions(transactions: any): void{
+    this.transactionsHistorySubject.next(transactions);
+  }
+
+  getTransactionsHttp(): Observable<any>{
+    return this.historyHttpService.getTransactionsHttp();
+  }
 }
--- src/app/features/transactions/pages/transactions-history/transactions-history.page.ts
+++ src/app/features/transactions/pages/transactions-history/transactions-history.page.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit, signal } from "@angular/core";
+import { HistoryService } from "./services/history.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-transactions-history",
@@ -6,4 +8,24 @@ import { Component } from "@angular/core";
   styleUrls: ["./transactions-history.page.scss"],
   standalone: false
 })
-export class TransactionsHistoryPage {}
+export class TransactionsHistoryPage implements OnInit, OnDestroy {
+  transactions = signal<any>(null);
+  private transactionsSubscription!: Subscription;
+
+  constructor(private historyService: HistoryService){}
+
+  ngOnInit(): void {
+
+  }
+
+
+  subscribeToTransactions(){
+    this.transactionsSubscription = this.historyService.getTransactions$.subscribe(
+      trans => this.transactions.set(trans)
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.transactionsSubscription?.unsubscribe();
+  }
+}
