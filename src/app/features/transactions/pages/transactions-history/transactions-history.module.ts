71fee564e1cdd8a9ea197f79626c27412b0e4fd0
Salim <doshka23@gmail.com>
2026-04-28 17:15:47 +0200

    integrated: history filter

--- src/app/features/transactions/pages/transactions-history/transactions-history.module.ts
+++ src/app/features/transactions/pages/transactions-history/transactions-history.module.ts
@@ -3,9 +3,11 @@ import { TransactionsHistoryPage } from "./transactions-history.page";
 import { TransactionsHistoryRoutingModule } from "./transactions-history-routing.module";
 import { HistoryHeaderComponent } from "./components/history-header/history-header.component";
 import { HistoryFilterComponent } from "./components/history-filter/history-filter.component";
+import { HistoryItemComponent } from "./components/history-item/history-item.component";
 
 @NgModule({
   declarations: [
+    HistoryItemComponent,
     HistoryFilterComponent,
     HistoryHeaderComponent,
     TransactionsHistoryPage,
