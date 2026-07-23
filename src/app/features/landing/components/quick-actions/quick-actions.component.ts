608e24373c131a18fe3de7c3dbf68d804b6853f2
Salim <doshka23@gmail.com>
2026-07-23 03:35:17 +0200

    built: QuickActionsComponent

---
+++ src/app/features/landing/components/quick-actions/quick-actions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "landing-quick-actions",
+  templateUrl: "./quick-actions.component.html",
+  styleUrls: ["./quick-actions.component.scss"],
+  standalone: false
+})
+
+export class QuickActionsComponent {}
