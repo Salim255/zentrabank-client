4c7c8130a63dd8b4b8d58efb11647f58f71e5ad1
Salim <doshka23@gmail.com>
2026-07-23 03:32:56 +0200

    built: SpendingChartComponent

---
+++ src/app/features/landing/components/spending-chart/spending-chart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "landing-spending-chart",
+  templateUrl: "./spending-chart.component.html",
+  styleUrls: ["./spending-chart.component.scss"],
+  standalone: false
+})
+
+export class SpendingChartComponent {}
