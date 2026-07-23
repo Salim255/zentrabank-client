820b95283c69313034826065b95ffc64ec941826
Salim <doshka23@gmail.com>
2026-07-23 04:45:16 +0200

    built:  HeroSectionComponent

---
+++ src/app/features/landing/components/hero-section/hero-section.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "landing-hero-section",
+  templateUrl: "./hero-section.component.html",
+  styleUrls: ["./hero-section.component.scss"],
+  standalone: false
+})
+export class HeroSectionComponent {}
