9dece73e6651dfa501b4dc78426c2d6855803df4
Salim <doshka23@gmail.com>
2026-07-23 03:44:16 +0200

    built:DashboardPreviewComponent

---
+++ src/app/features/landing/components/dashboard-preview/dashboard-preview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "landing-dashboard-preview",
+  templateUrl: "./dashboard-preview.component.html",
+  styleUrls: ["./dashboard-preview.component.scss"],
+  standalone: false
+})
+
+export class DashboardPreviewComponent {}
