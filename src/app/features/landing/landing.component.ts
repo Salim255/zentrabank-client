946c12a6c8569f00491c67e1fd2c0180ac5b7a7c
Salim <doshka23@gmail.com>
2026-07-23 03:18:12 +0200

    built: LandingRoutingModule

---
+++ src/app/features/landing/landing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-lading",
+  templateUrl: "./landing.component.html",
+  styleUrls: ["./landing.component.scss"],
+  standalone: false
+})
+
+export class LandingComponent {}
