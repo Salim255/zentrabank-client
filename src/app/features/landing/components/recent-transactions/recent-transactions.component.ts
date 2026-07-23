b875660a7d6f637b0a88370344afd27909688f09
Salim <doshka23@gmail.com>
2026-07-23 03:37:36 +0200

    built:  RecentTransactionsComponent

---
+++ src/app/features/landing/components/recent-transactions/recent-transactions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "landing-recent-transactions",
+  templateUrl: "./recent-transactions.component.html",
+  styleUrls: ["./recent-transactions.component.scss"],
+  standalone: false
+})
+
+export class RecentTransactionsComponent {}
