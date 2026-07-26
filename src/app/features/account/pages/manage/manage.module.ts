2bced3242e10e5c6d0863177f2791cbe72f698bd
Salim <doshka23@gmail.com>
2026-07-26 12:00:17 +0200

    some style

--- src/app/features/account/pages/manage/manage.module.ts
+++ src/app/features/account/pages/manage/manage.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { ManageRoutingModule } from "./manage-routing.module";
 import { ManagePage } from "./manage.page";
 import { SharedModule } from "../../../../shared/shared.module";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
-  imports: [SharedModule, ManageRoutingModule],
+  imports: [CommonModule ,SharedModule, ManageRoutingModule],
   declarations: [ManagePage]
 })
 
